bbeed9bda9019b6a303709825d009f7016d1cb24
shtigran <shakhbekyantigran@gmail.com>
2017-01-30 00:06:44 +0400

    Text

--- ArmenianTranslitTranslator/ArmenianTranslitTranslator/Form1.Designer.cs
+++ ArmenianTranslitTranslator/ArmenianTranslitTranslator/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(102, 90);
+            this.label1.Location = new System.Drawing.Point(104, 88);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(163, 23);
             this.label1.TabIndex = 1;
