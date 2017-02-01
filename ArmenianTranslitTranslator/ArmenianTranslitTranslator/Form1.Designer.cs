5f7d9aba0c416ac478a94e94093311979a0d1c24
User <User@COMP>
2017-02-01 10:59:02 +0400

    File input cheking

--- ArmenianTranslitTranslator/ArmenianTranslitTranslator/Form1.Designer.cs
+++ ArmenianTranslitTranslator/ArmenianTranslitTranslator/Form1.Designer.cs
@@ -28,65 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(104, 88);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Select the text file ";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label1.UseWaitCursor = true;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Font = new System.Drawing.Font("Times Unicode", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(-5, 22);
-            this.label2.Name = "label2";
-            this.label2.Padding = new System.Windows.Forms.Padding(4);
-            this.label2.Size = new System.Drawing.Size(395, 32);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Ա Բ Գ Դ Ե Զ Է Ը --> A B C D E F G H I ";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(33, 122);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(311, 23);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "To translate into Armenian Translit";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(380, 417);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Armenian Translit Translator";
-            this.TopMost = true;
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.Select = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.label1.Location = new System.Drawing.Point(104, 88);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(163, 23);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Select the text file ";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+      this.label1.UseWaitCursor = true;
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.label2.Font = new System.Drawing.Font("Times Unicode", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.label2.Location = new System.Drawing.Point(-5, 22);
+      this.label2.Name = "label2";
+      this.label2.Padding = new System.Windows.Forms.Padding(4);
+      this.label2.Size = new System.Drawing.Size(395, 32);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Ա Բ Գ Դ Ե Զ Է Ը --> A B C D E F G H I ";
+      this.label2.Click += new System.EventHandler(this.label2_Click);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.label3.Location = new System.Drawing.Point(33, 122);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(311, 23);
+      this.label3.TabIndex = 3;
+      this.label3.Text = "To translate into Armenian Translit";
+      this.label3.Click += new System.EventHandler(this.label3_Click_1);
+      // 
+      // Select
+      // 
+      this.Select.Location = new System.Drawing.Point(37, 178);
+      this.Select.Name = "Select";
+      this.Select.Size = new System.Drawing.Size(82, 32);
+      this.Select.TabIndex = 4;
+      this.Select.Text = "Select File";
+      this.Select.UseVisualStyleBackColor = true;
+      this.Select.Click += new System.EventHandler(this.Select_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.textBox1.Location = new System.Drawing.Point(200, 178);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(144, 32);
+      this.textBox1.TabIndex = 5;
+      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+      this.ClientSize = new System.Drawing.Size(380, 417);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.Select);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "Form1";
+      this.Text = "Armenian Translit Translator";
+      this.TopMost = true;
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
@@ -95,6 +120,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-    }
+    private System.Windows.Forms.Button Select;
+    private System.Windows.Forms.TextBox textBox1;
+  }
 }
 
--- ArmenianTranslitTranslator/ArmenianTranslitTranslator/Form1.cs
+++ ArmenianTranslitTranslator/ArmenianTranslitTranslator/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace ArmenianTranslitTranslator
 {
@@ -36,5 +38,55 @@ namespace ArmenianTranslitTranslator
         {
 
         }
+
+    private void Select_Click(object sender, EventArgs e)
+    {
+      Stream myStream = null;
+      OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+      openFileDialog1.InitialDirectory = "c:\\";
+      openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+      openFileDialog1.RestoreDirectory = true;
+      openFileDialog1.FilterIndex = 1;
+
+
+      if (openFileDialog1.ShowDialog() == DialogResult.OK)
+      {
+        try
+        {
+          if ((myStream = openFileDialog1.OpenFile()) != null)
+          {
+            using (myStream)
+            {
+
+              if (openFileDialog1.SafeFileName.Contains(".txt"))
+                   textBox1.Text = "True file format (.txt)";
+              else textBox1.Text = "Wrong file format (file must has .txt extension), check it!";
+
+            }
+          }
+        }
+        catch (Exception ex)
+        {
+          textBox1.Text = "This is a TextBox Error.";
+          MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+        }
+      }
     }
+
+    private void label3_Click_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
 }
